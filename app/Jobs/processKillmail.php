6546d8da115dc1ff718e91c55c8e2a6dc4b03152
Michael Karbowiak <michael@karbowiak.dk>
2024-06-01 20:53:26 +0200

    Clean up the processKillmail job, because it doesn't _NEED_ to fetch the mail from ESI, the parser does that

--- app/Jobs/processKillmail.php
+++ app/Jobs/processKillmail.php
@@ -3,9 +3,7 @@
 namespace EK\Jobs;
 
 use EK\Api\Abstracts\Jobs;
-use EK\ESI\EsiFetcher;
 use EK\Models\Killmails;
-use EK\Models\KillmailsESI;
 use EK\Redis\Redis;
 
 class processKillmail extends Jobs
@@ -13,9 +11,6 @@ class processKillmail extends Jobs
     protected string $defaultQueue = 'killmail';
     public function __construct(
         protected Killmails $killmails,
-        protected KillmailsESI $killmailsESI,
-        protected \EK\ESI\Killmails $esiKillmails,
-        protected EsiFetcher $esiFetcher,
         protected \EK\Helpers\Killmails $killmailHelper,
         protected Redis $redis
     ) {
@@ -27,19 +22,6 @@ class processKillmail extends Jobs
         $killmail_id = $data['killmail_id'];
         $hash = $data['hash'];
         $war_id = $data['war_id'] ?? 0;
-
-        // Get the killmail data from the killmail model
-        $killmail = $this->killmails->findOneOrNull(['killmail_id' => $killmail_id, 'hash' => $hash]);
-        if (!$killmail) {
-            throw new \RuntimeException('Killmail with id: ' . $killmail_id . ' not found');
-        }
-
-        // Get the killmail data from the ESI
-        $killmailData = $this->esiKillmails->getKillmail($killmail_id, $hash);
-
-        // Insert the killmail data into the esi killmails collection
-        $this->killmailsESI->setData($killmailData);
-        $this->killmailsESI->save();
 
         // Parse the killmail
         $parsedKillmail = $this->killmailHelper->parseKillmail($killmail_id, $hash, $war_id);
