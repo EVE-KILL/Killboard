6337aa09529581d1b1035973039ad49dfaee3ee3
Michael Karbowiak <michael@karbowiak.dk>
2024-06-10 00:40:30 +0200

    Add ability to post killids to /api/killmails

--- app/Controllers/Api/Killmail.php
+++ app/Controllers/Api/Killmail.php
@@ -37,4 +37,24 @@ class Killmail extends Controller
 
         return $this->json($this->cleanupTimestamps($killmail->toArray()));
     }
+
+    #[RouteAttribute('/killmail[/]', ['POST'])]
+    public function killmails(): ResponseInterface
+    {
+        $postData = json_validate($this->getBody()) ? json_decode($this->getBody(), true) : [];
+        if (empty($postData)) {
+            return $this->json(['error' => 'No data provided'], 300);
+        }
+
+        // Error if there are more than 1000 IDs
+        if (count($postData) > 1000) {
+            return $this->json(['error' => 'Too many IDs provided'], 300);
+        }
+
+        $killmails = $this->killmails->find(['killmail_id' => ['$in' => $postData]], ['projection' => ['_id' => 0]], 300)->map(function ($killmail) {
+            return $this->cleanupTimestamps($killmail);
+        });
+
+        return $this->json($killmails->toArray(), 300);
+    }
 }
