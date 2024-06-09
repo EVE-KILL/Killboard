e90c168ccf119f24d1be6308d40f818f3c2ff15a
Michael Karbowiak <michael@karbowiak.dk>
2024-06-10 00:00:47 +0200

    Add api endpoint for getting killmail data

---
+++ app/Controllers/Api/Killmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EK\Controllers\Api;
+
+use EK\Api\Abstracts\Controller;
+use EK\Api\Attributes\RouteAttribute;
+use EK\Http\Twig\Twig;
+use Psr\Http\Message\ResponseInterface;
+
+class Killmail extends Controller
+{
+    public function __construct(
+        protected \EK\Models\Killmails $killmails,
+        protected Twig $twig
+    ) {
+        parent::__construct($twig);
+    }
+
+    #[RouteAttribute('/killmail/count[/]', ['GET'])]
+    public function count(): ResponseInterface
+    {
+        return $this->json([
+            'count' => $this->killmails->count(),
+        ]);
+    }
+
+    #[RouteAttribute('/killmail/{killmail_id:[0-9]+}[/]', ['GET'])]
+    public function killmail(int $killmail_id): ResponseInterface
+    {
+        $killmail = $this->killmails->get($killmail_id);
+
+        if ($killmail === null) {
+            return $this->json([
+                'error' => 'Killmail not found',
+            ], 404);
+        }
+
+        return $this->json($killmail);
+    }
+}
