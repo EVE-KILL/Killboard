88fd9256c639daa9d1a920d46d8e3dd553087593
Michael Karbowiak <michael@karbowiak.dk>
2024-06-13 16:04:55 +0200

    Cleanup the timestamps when emitting mails to the websocket

--- app/Jobs/emitKillmailWS.php
+++ app/Jobs/emitKillmailWS.php
@@ -5,6 +5,7 @@ namespace EK\Jobs;
 use EK\Api\Abstracts\Jobs;
 use EK\Models\Killmails;
 use EK\Redis\Redis;
+use MongoDB\BSON\UTCDateTime;
 use WebSocket\Client;
 
 class emitKillmailWS extends Jobs
@@ -24,7 +25,25 @@ class emitKillmailWS extends Jobs
         $client->text(json_encode([
             'type' => 'broadcast',
             'token' => 'my-secret',
-            'data' => $data
+            'data' => $this->cleanupTimestamps($data)
         ]));
+    }
+
+    private function cleanupTimestamps(array $data): array
+    {
+        foreach ($data as $key => $value) {
+            if ($value instanceof UTCDateTime) {
+                $data[$key] = $value->toDateTime()->format('Y-m-d H:i:s');
+            }
+
+            // Sometimes we don't get an array with proper instances, sometimes we get an array with $date and $numberLong nested under each other
+            if (is_array($value)) {
+                if (isset($value['$date']['$numberLong']) && is_array($value['$date'])) {
+                    $data[$key] = (new UTCDateTime($value['$date']['$numberLong']))->toDateTime()->format('Y-m-d H:i:s');
+                }
+            }
+        }
+
+        return $data;
     }
 }
