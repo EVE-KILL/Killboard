022015df85c4cd8b71cc71000810a0b0b1d8fe62
Michael Karbowiak <michael@karbowiak.dk>
2024-08-07 19:52:21 +0200

    Add a characterscrape job that keeps queueing up increments of 10 to try and find new characters

---
+++ app/Cronjobs/CharacterScraper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace EK\Cronjobs;
+
+use EK\Api\Abstracts\Cronjob;
+use EK\Jobs\CharacterScrape;
+use EK\Jobs\UpdateCharacter;
+use EK\Models\Characters;
+
+class CharacterScraper extends Cronjob
+{
+    protected string $cronTime = '* * * * *';
+
+    public function __construct(
+        protected Characters $characters,
+        protected CharacterScrape $characterScrape
+    ) {
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        // Get the biggest characterId from the database
+        $largestCharacterId = $this->characters->findOne([], ['sort' => ['character_id' => -1]])['character_id'] ?? 0;
+
+        // Generate an array of characterIds to scrape (Largest +100)
+        $characterIds = range($largestCharacterId + 1, $largestCharacterId + 10);
+
+        // Enqueue the character update jobs
+        $this->characterScrape->massEnqueue(array_map(fn($characterId) => ['character_id' => $characterId], $characterIds));
+    }
+}
