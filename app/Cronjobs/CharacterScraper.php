86a2a9dd3c070740506ea1d8b6da378d44ac7853
Michael Karbowiak <michael@karbowiak.dk>
2024-08-08 20:59:24 +0200

    Raise the ids checked to 25

--- app/Cronjobs/CharacterScraper.php
+++ app/Cronjobs/CharacterScraper.php
@@ -25,7 +25,7 @@ class CharacterScraper extends Cronjob
         $largestCharacterId = $this->characters->findOne([], ['sort' => ['character_id' => -1]])['character_id'] ?? 0;
 
         // Generate an array of characterIds to scrape (Largest +100)
-        $characterIds = range($largestCharacterId + 1, $largestCharacterId + 5);
+        $characterIds = range($largestCharacterId + 1, $largestCharacterId + 25);
 
         // Enqueue the character update jobs
         $this->characterScrape->massEnqueue(array_map(fn($characterId) => ['character_id' => $characterId], $characterIds));
