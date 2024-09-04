74e3c32b6cdca1382234cfcb0cf8dc537cd7b42d
Michael Karbowiak <michael@karbowiak.dk>
2024-09-04 23:00:46 +0200

    Add campaigns scheduling cronjob so they get processed when they're over an hour old

---
+++ app/Cronjobs/Campaigns.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EK\Cronjobs;
+
+use EK\Api\Abstracts\Cronjob;
+use EK\Jobs\ProcessCampaign;
+use EK\Logger\StdOutLogger;
+use EK\Models\Campaigns as CampaignsModel;
+
+class Campaigns extends Cronjob
+{
+    protected string $cronTime = '* * * * *';
+
+    public function __construct(
+        protected StdOutLogger $logger,
+        protected CampaignsModel $campaignsModel,
+        protected ProcessCampaign $processCampaign
+    ) {
+        parent::__construct($logger);
+    }
+
+    public function handle(): void
+    {
+        // Find campaigns that have a last_modified older than 1 hour
+        $this->campaignsModel->find([
+            'last_modified' => ['$lt' => new \MongoDB\BSON\UTCDateTime(strtotime('-1 hour') * 1000)]
+        ])->each(function ($campaign) {
+            $this->processCampaign->enqueue(['campaign_id' => $campaign['campaign_id']]);
+        });
+    }
+}
