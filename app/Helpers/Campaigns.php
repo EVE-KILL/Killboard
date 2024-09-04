293f2ceef525c4cc44ac75077aba9fa8595e89f0
Michael Karbowiak <michael@karbowiak.dk>
2024-09-05 00:21:20 +0200

    Calculate the stats correctly instead of the whack output before

--- app/Helpers/Campaigns.php
+++ app/Helpers/Campaigns.php
@@ -134,8 +134,7 @@ class Campaigns
                 'region_id' => 1,
                 'attackers' => 1,
                 'victim' => 1,
-                'value_killed' => 1,
-                'value_lost' => 1
+                'total_value' => 1
             ]
         ];
 
@@ -143,77 +142,203 @@ class Campaigns
         $cursor = $this->killmails->collection->aggregate($pipeline);
 
         // Initialize stats
-        $kills = 0;
-        $losses = 0;
-        $totalValueKilled = 0;
-        $totalValueLost = 0;
-        $activePilots = [];
-        $systemActivity = [];
-        $regionActivity = [];
-        $shipUsage = [];
+        $totalKills = 0;
+        // Kills
+        $friendlyKills = 0;
+        $foeKills = 0;
+        // Losses
+        $friendlyLosses = 0;
+        $foeLosses = 0;
+        // Total Value
+        $totalValue = 0;
+        $friendlyTotalValue = 0;
+        $foeTotalValue = 0;
+        // Pilots
+        $friendlyActivePilots = [];
+        $foeActivePilots = [];
+        // Corporations
+        $friendlyActiveCorporations = [];
+        $foeActiveCorporations = [];
+        // Alliances
+        $friendlyActiveAlliances = [];
+        $foeActiveAlliances = [];
+        // Systems
+        $activeSystems = [];
+        $friendlyActiveSystems = [];
+        $foeActiveSystems = [];
+        // Regions
+        $activeRegions = [];
+        $friendlyActiveRegions = [];
+        $foeActiveRegions = [];
+        // Ships
+        $usedShips = [];
+        $friendlyUsedShips = [];
+        $foeUsedShips = [];
+        // Ship Gruops
+        $usedShipGroups = [];
+        $friendlyUsedShipGroups = [];
+        $foeUsedShipGroups = [];
+        // Specialties
+        $soloKills = 0;
+        $npcKills = 0;
 
         // Process each killmail in the cursor to compute stats
         foreach ($cursor as $killmail) {
-            // Increment kills if attackers are present
-            if (!empty($killmail['attackers'])) {
-                $kills++;
-                $totalValueKilled += $killmail['value_killed'] ?? 0;
+            // Every kill goes into $totalKills
+            $totalKills++;
+            $totalValue += $killmail['total_value'];
+            $activeSystems[$killmail['system_id']] = true;
+            $activeRegions[$killmail['region_id']] = true;
+            foreach($killmail['attackers'] as $attacker) {
+                $usedShips[$attacker['ship_id']] = true;
+                $usedShipGroups[$attacker['ship_group_id']] = true;
             }
+            $usedShips[$killmail['victim']['ship_id']] = true;
+            $usedShipGroups[$killmail['victim']['ship_group_id']] = true;
 
-            // Increment losses if the victim is present
-            if (!empty($killmail['victim'])) {
-                $losses++;
-                $totalValueLost += $killmail['value_lost'] ?? 0;
-            }
+            //
+            if (!empty($entities)) {
+                foreach ($entities as $entity) {
+                    $id = $entity['id'];
+                    $type = $entity['type'];
+                    $treatment = $entity['treatment'];
 
-            // Add active pilots from attackers
-            foreach ($killmail['attackers'] as $attacker) {
-                if (isset($attacker['character_id'])) {
-                    $activePilots[$attacker['character_id']] = true; // Use associative array to ensure uniqueness
-                }
-            }
+                    switch ($treatment) {
+                        case 'friend':
+                                $friendlyKills++;
+                                $friendlyTotalValue += $killmail['total_value'];
+                                $friendlyActiveSystems[$killmail['system_id']] = true;
+                                $friendlyActiveRegions[$killmail['region_id']] = true;
 
-            // Track system activity
-            if (!empty($killmail['system_id'])) {
-                if (!isset($systemActivity[$killmail['system_id']])) {
-                    $systemActivity[$killmail['system_id']] = 0;
-                }
-                $systemActivity[$killmail['system_id']]++;
-            }
+                                if ($killmail['victim'][$type . '_id'] === $id) {
+                                    $friendlyLosses++;
+                                }
 
-            // Track region activity
-            if (!empty($killmail['region_id'])) {
-                if (!isset($regionActivity[$killmail['region_id']])) {
-                    $regionActivity[$killmail['region_id']] = 0;
-                }
-                $regionActivity[$killmail['region_id']]++;
-            }
+                                foreach($killmail['attackers'] as $attacker) {
+                                    switch($type . '_id') {
+                                        case 'character_id':
+                                            if ($attacker['character_id'] === $id) {
+                                                $friendlyUsedShips[$attacker['ship_id']] = true;
+                                                $friendlyUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'corporation_id':
+                                            if ($attacker['corporation_id'] === $id) {
+                                                $friendlyActivePilots[$attacker['character_id']] = true;
+                                                $friendlyUsedShips[$attacker['ship_id']] = true;
+                                                $friendlyUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'alliance_id':
+                                            if ($attacker['alliance_id'] === $id) {
+                                                $friendlyActivePilots[$attacker['character_id']] = true;
+                                                $friendlyActiveCorporations[$attacker['corporation_id']] = true;
+                                                $friendlyActiveAlliances[$attacker['alliance_id']] = true;
+                                                $friendlyUsedShips[$attacker['ship_id']] = true;
+                                                $friendlyUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'ship_id':
+                                            if ($attacker['ship_id'] === $id) {
+                                                $friendlyActivePilots[$attacker['character_id']] = true;
+                                                $friendlyUsedShips[$attacker['ship_id']] = true;
+                                                $friendlyUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                    }
+                                }
+                            break;
+                        case 'foe':
+                                $foeKills++;
+                                $foeTotalValue += $killmail['total_value'];
+                                $foeActiveSystems[$killmail['system_id']] = true;
+                                $foeActiveRegions[$killmail['region_id']] = true;
 
-            // Track ship usage from attackers
-            foreach ($killmail['attackers'] as $attacker) {
-                if (!empty($attacker['ship_id'])) {
-                    if (!isset($shipUsage[$attacker['ship_id']])) {
-                        $shipUsage[$attacker['ship_id']] = 0;
+                                if ($killmail['victim'][$type . '_id'] === $id) {
+                                    $foeLosses++;
+                                }
+
+                                foreach($killmail['attackers'] as $attacker) {
+                                    switch($type . '_id') {
+                                        case 'character_id':
+                                            if ($attacker['character_id'] === $id) {
+                                                $foeUsedShips[$attacker['ship_id']] = true;
+                                                $foeUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'corporation_id':
+                                            if ($attacker['corporation_id'] === $id) {
+                                                $foeActivePilots[$attacker['character_id']] = true;
+                                                $foeUsedShips[$attacker['ship_id']] = true;
+                                                $foeUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'alliance_id':
+                                            if ($attacker['alliance_id'] === $id) {
+                                                $foeActivePilots[$attacker['character_id']] = true;
+                                                $foeActiveCorporations[$attacker['corporation_id']] = true;
+                                                $foeActiveAlliances[$attacker['alliance_id']] = true;
+                                                $foeUsedShips[$attacker['ship_id']] = true;
+                                                $foeUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                        case 'ship_id':
+                                            if ($attacker['ship_id'] === $id) {
+                                                $foeActivePilots[$attacker['character_id']] = true;
+                                                $foeUsedShips[$attacker['ship_id']] = true;
+                                                $foeUsedShipGroups[$attacker['ship_group_id']] = true;
+                                            }
+                                        break;
+                                    }
+                                }
+                            break;
+                        case 'solo':
+                            if ($killmail['is_solo']) {
+                                $soloKills++;
+                            }
+                            break;
+                        case 'npc':
+                            if ($killmail['is_npc']) {
+                                $npcKills++;
+                            }
+                            break;
                     }
-                    $shipUsage[$attacker['ship_id']]++;
                 }
             }
         }
 
         // Post-processing the stats
         $campaign['stats'] = [
-            'kills' => $kills,
-            'losses' => $losses,
-            'value_killed' => $totalValueKilled,
-            'value_lost' => $totalValueLost,
-            'active_pilots' => count($activePilots), // Unique active pilots
-            'most_active_system' => !empty($systemActivity) ? array_search(max($systemActivity), $systemActivity) : null,
-            'most_active_region' => !empty($regionActivity) ? array_search(max($regionActivity), $regionActivity) : null,
-            'most_used_ship' => !empty($shipUsage) ? array_search(max($shipUsage), $shipUsage) : null,
+            'totalKills' => $totalKills,
+            'friendlyKills' => $friendlyKills,
+            'friendlyLosses' => $friendlyLosses,
+            'foeKills' => $foeKills,
+            'foeLosses' => $foeLosses,
+            'totalValue' => $totalValue,
+            'friendlyTotalValue' => $friendlyTotalValue,
+            'foeTotalValue' => $foeTotalValue,
+            'friendlyActivePilots' => count($friendlyActivePilots),
+            'foeActivePilots' => count($foeActivePilots),
+            'friendlyActiveCorporations' => count($friendlyActiveCorporations),
+            'foeActiveCorporations' => count($foeActiveCorporations),
+            'friendlyActiveAlliances' => count($friendlyActiveAlliances),
+            'foeActiveAlliances' => count($foeActiveAlliances),
+            'activeSystems' => count($activeSystems),
+            'friendlyActiveSystems' => count($friendlyActiveSystems),
+            'foeActiveSystems' => count($foeActiveSystems),
+            'activeRegions' => count($activeRegions),
+            'friendlyActiveRegions' => count($friendlyActiveRegions),
+            'foeActiveRegions' => count($foeActiveRegions),
+            'usedShips' => count($usedShips),
+            'friendlyUsedShips' => count($friendlyUsedShips),
+            'foeUsedShips' => count($foeUsedShips),
+            'usedShipGroups' => count($usedShipGroups),
+            'friendlyUsedShipGroups' => count($friendlyUsedShipGroups),
+            'foeUsedShipGroups' => count($foeUsedShipGroups),
+            'soloKills' => $soloKills,
+            'npcKills' => $npcKills
         ];
 
         return $campaign;
     }
-
-
 }
