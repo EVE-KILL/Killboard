be4c2fd8d9e27242d9b24d76537c46da8c45575f
Michael Karbowiak <michael@karbowiak.dk>
2024-09-28 00:32:10 +0200

    Add intel controller to.. get intel..

---
+++ app/Controllers/Api/Intel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace EK\Controllers\Api;
+
+use EK\Api\Abstracts\Controller;
+use EK\Api\Attributes\RouteAttribute;
+use EK\Models\Killmails;
+use Psr\Http\Message\ResponseInterface;
+
+class Intel extends Controller
+{
+    public function __construct(
+        protected Killmails $killmails
+    ) {
+        parent::__construct();
+
+    }
+
+    #[RouteAttribute("/intel/metenoxMoons", ["GET"], "Get Metenox moon locations based on killmails")]
+    public function metenoxMoons(): ResponseInterface
+    {
+        $metenoxId = 81826;
+        $killmails = $this->killmails->find(['victim.ship_id' => $metenoxId]);
+
+        $killmails = $this->cleanupTimestamps($killmails);
+
+        return $this->json($killmails);
+    }
+
+}
