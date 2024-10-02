df528198c75665227b5aac67ab13fed9ba5e673f
Michael Karbowiak <michael@karbowiak.dk>
2024-10-02 22:07:14 +0200

    Raise history fetch limit to 20/s

--- app/Fetchers/History.php
+++ app/Fetchers/History.php
@@ -7,5 +7,5 @@ class History extends ESI
     protected string $bucketName = 'esi_history';
 
     // Rate limit the character/*/corporationhistory and corporation/*/alliancehistory endpoints to 20 requests per second
-    protected int $rateLimit = 10;
+    protected int $rateLimit = 20;
 }
