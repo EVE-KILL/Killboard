a60f47b3d29a9bc176ec99f8e86e61055f80639f
Michael Karbowiak <michael@karbowiak.dk>
2024-10-05 03:06:26 +0200

    Use the history fetcher to rate limit the history endpoints separately

--- app/Fetchers/History.php
+++ app/Fetchers/History.php
@@ -5,7 +5,5 @@ namespace EK\Fetchers;
 class History extends ESI
 {
     protected string $bucketName = 'esi_history';
-
-    // Rate limit the character/*/corporationhistory and corporation/*/alliancehistory endpoints to 20 requests per second
     protected int $rateLimit = 30;
 }
